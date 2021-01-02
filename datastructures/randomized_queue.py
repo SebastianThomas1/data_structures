4c45aba578305681d2c31e4d397c54a41cd32113
Sebastian Thomas <sigma.online@gmx.de>
2021-01-02 11:41:58 +0100

    added RandomizedQueue

---
+++ datastructures/randomized_queue.py
@@ -0,0 +1,244 @@
+# Sebastian Thomas (datascience at sebastianthomas dot de)
+
+# abstract base classes
+from abc import abstractmethod
+from collections.abc import Iterable
+
+# representations of objects
+from reprlib import repr as reprlib_repr
+
+# randomization
+from random import choice, randrange, seed
+from numpy.random import permutation, seed as np_seed
+
+# custom modules
+from datastructures.base import Collection
+from datastructures.node import LinkedNode
+
+
+__all__ = ['RandomizedQueue', 'ArrayRandomizedQueue', 'LinkedRandomizedQueue',
+           'EmptyRandomizedQueueException']
+
+
+class RandomizedQueue(Collection):
+    """Abstract base class for the abstract data type randomized queue.
+
+    Concrete subclasses must provide: __new__ or __init__, __iter__, sample,
+    enqueue, dequeue."""
+
+    def __copy__(self):
+        copy_of_self = type(self)()
+        copy_of_self += self
+        return copy_of_self
+
+    def __iadd__(self, other):
+        if not isinstance(other, Iterable):
+            raise TypeError('\'{}\' object is not iterable.'.format(type(other)
+                                                                    .__name__))
+
+        for value in other:
+            self.enqueue(value)
+
+        return self
+
+    @abstractmethod
+    def choice(self):
+        """Returns a random item of the randomized queue (but does not remove
+        it)."""
+        pass
+
+    @abstractmethod
+    def enqueue(self, value):
+        """Enqueues an item on the randomized_queue."""
+        pass
+
+    @abstractmethod
+    def dequeue(self):
+        """Dequeues a random item from the randomized queue."""
+        pass
+
+
+class ArrayRandomizedQueue(RandomizedQueue):
+    """Class that implements randomized queues based on an internal dynamic
+    array (python list)."""
+
+    def __init__(self, random_state=None):
+        self._values = []
+        self.random_state = random_state
+        seed(random_state)
+
+    def __iter__(self):
+        np_seed(self.random_state)
+        for idx in permutation(len(self)):
+            yield self._values[idx]
+
+    def __len__(self):
+        return len(self._values)
+
+    def __repr__(self):
+        return '{}({})'.format(type(self).__name__,
+                               reprlib_repr(self._values))
+
+    def __str__(self):
+        return ' '.join(str(value) for value in self._values)
+
+    def __contains__(self, value):
+        return value in self._values
+
+    def __iadd__(self, other):
+        if not isinstance(other, Iterable):
+            raise TypeError('\'{}\' object is not iterable.'.format(type(other)
+                                                                    .__name__))
+
+        self._values += other
+
+        return self
+
+    def is_empty(self):
+        """Checks whether this instance is an empty queue."""
+        return not bool(self._values)
+
+    def choice(self):
+        """Returns a random item of the randomized queue (but does not remove
+        it)."""
+        if self.is_empty():
+            raise EmptyRandomizedQueueException('Can\'t access entries of '
+                                                'empty randomized queue.')
+
+        return choice(self._values)
+
+    def enqueue(self, value):
+        """Enqueues an item on the randomized queue."""
+        self._values.append(value)
+
+    def dequeue(self):
+        """Dequeues a random item from the randomized queue."""
+        if self.is_empty():
+            raise EmptyRandomizedQueueException('Can\'t dequeue from empty '
+                                                'randomized queue.')
+
+        return self._values.pop(randrange(len(self)))
+
+
+class LinkedRandomizedQueue(RandomizedQueue):
+    """Class that implements randomized queues based on linked nodes."""
+
+    class Node(LinkedNode):
+        """Internal node class for linked randomized queues."""
+        pass
+
+    def __init__(self, random_state=None):
+        self._front = None
+        self._len = 0
+        self.random_state = random_state
+        seed(random_state)
+
+    def __iter__(self):
+        np_seed(self.random_state)
+        for idx in permutation(len(self)):
+            yield self._get_node(idx).value
+
+    def __repr__(self):
+        # determine first seven values (at most)
+        first_values = []
+        count = 0
+        for node in self._traversal():
+            first_values.append(node.value)
+            count += 1
+            if count == 7:
+                break
+
+        return '{}({})'.format(type(self).__name__,
+                               reprlib_repr(first_values))
+
+    def __str__(self):
+        return ' '.join(str(node.value) for node in self._traversal())
+
+    def __len__(self):
+        return self._len
+
+    def _traversal(self, start_node=None):
+        """Traverses instance, beginning with start_node (default: head)."""
+        if start_node is None:
+            start_node = self._front
+
+        current_node = start_node
+        while current_node:
+            yield current_node
+            current_node = current_node.successor
+
+    def _get_node(self, key):  # assume key is an integer
+        """Returns node at index."""
+        if self.is_empty():
+            raise IndexError('Can\'t access index in empty randomized queue.')
+
+        # traverse instance, return current node if item at index is
+        # reached
+        for node in self._traversal():
+            if key == 0:
+                return node
+            key -= 1
+
+        raise IndexError('Index out of range.')
+
+    def _get_node_with_predecessor(self, key):
+        """Returns node at index together with predecessor."""
+        if self.is_empty():
+            raise IndexError('Can\'t access index in empty randomized queue.')
+
+        # traverse instance, return current node and predecessor if item
+        # at index is reached
+        predecessor = None
+        for node in self._traversal():
+            if key == 0:
+                return node, predecessor
+            key -= 1
+            predecessor = node
+
+        raise IndexError('Index out of range.')
+
+    def _remove_node(self, node, predecessor):
+        """Removes node by connecting predecessor with successor."""
+        if predecessor:
+            predecessor.successor = node.successor
+        else:  # ie node is self._front
+            self._front = self._front.successor
+
+        self._len -= 1
+
+    def is_empty(self):
+        """Checks whether this instance is an empty queue."""
+        return self._front is None
+
+    def choice(self):
+        """Returns a random item of the randomized queue (but does not remove
+        it)."""
+        if self.is_empty():
+            raise EmptyRandomizedQueueException('Can\'t access entries of '
+                                                'empty randomized queue.')
+
+        return self._get_node(randrange(len(self))).value
+
+    def enqueue(self, value):
+        """Enqueues an item on the randomized queue."""
+        self._front = self.Node(value, self._front)
+        self._len += 1
+
+    def dequeue(self):
+        """Dequeues a random item from the randomized queue."""
+        if self.is_empty():
+            raise EmptyRandomizedQueueException('Can\'t dequeue from empty '
+                                                'randomized queue.')
+
+        node, predecessor = \
+            self._get_node_with_predecessor(randrange(len(self)))
+        self._remove_node(node, predecessor)
+
+        return node.value
+
+
+# noch DoublyLinked machen? für schnelleren access?
+
+
+class EmptyRandomizedQueueException(Exception):
+    pass
---
+++ unittests/test_randomized_queue.py
@@ -0,0 +1,345 @@
+# Sebastian Thomas (datascience at sebastianthomas dot de)
+
+# custom modules
+from datastructures.randomized_queue import *
+
+# unit tests
+import unittest
+
+
+class TestRandomizedQueue(unittest.TestCase):
+    def __new__(cls, method_name, tested_class=None):
+        if cls is TestRandomizedQueue:
+            raise TypeError('Class TestRandomizedQueue may not be '
+                            'instantiated.')
+        return super().__new__(cls)
+
+    def __init__(self, method_name, tested_class=None):
+        super().__init__(methodName=method_name)
+        self.tested_class = tested_class
+
+    def setUp(self):
+        self.empty_queue = self.tested_class(random_state=0)
+
+        self.queue_length_1 = self.tested_class(random_state=0)
+        self.queue_length_1.enqueue(0)
+
+        self.range_queue = self.tested_class(random_state=0)
+        self.range_queue += range(4)
+
+        self.queue = self.tested_class(random_state=0)
+        self.queue += [1, 42, -3, 2, 42]
+
+    def test_eq(self):
+        queue = self.tested_class()
+        self.assertEqual(self.empty_queue, queue)
+        queue = self.tested_class()
+        queue += [0]
+        self.assertEqual(self.queue_length_1, queue)
+        queue = self.tested_class()
+        queue += range(4)
+        self.assertEqual(self.range_queue, queue)
+        queue = self.tested_class()
+        queue += [1, 42, -3, 2, 42]
+        self.assertEqual(self.queue, queue)
+
+        self.assertNotEqual(self.empty_queue, self.queue_length_1)
+        self.assertNotEqual(self.queue_length_1, self.range_queue)
+        self.assertNotEqual(self.range_queue, self.queue)
+        self.assertNotEqual(self.queue, self.empty_queue)
+        self.assertNotEqual(self.empty_queue, [])
+        self.assertNotEqual(self.queue_length_1, [0])
+        self.assertNotEqual(self.range_queue, range(4))
+        self.assertNotEqual(self.queue, [1, 42, -3, 2, 42])
+
+    def test_bool(self):
+        self.assertFalse(bool(self.empty_queue))
+        self.assertTrue(bool(self.queue_length_1))
+        self.assertTrue(bool(self.range_queue))
+        self.assertTrue(bool(self.queue))
+
+    def test_len(self):
+        self.assertEqual(len(self.empty_queue), 0)
+        self.assertEqual(len(self.queue_length_1), 1)
+        self.assertEqual(len(self.range_queue), 4)
+        self.assertEqual(len(self.queue), 5)
+
+    def test_contains(self):
+        self.assertFalse(0 in self.empty_queue)
+        self.assertTrue(0 in self.queue_length_1)
+        self.assertFalse(1 in self.queue_length_1)
+        self.assertFalse('0' in self.queue_length_1)
+        self.assertTrue(0 in self.range_queue)
+        self.assertTrue(1 in self.range_queue)
+        self.assertTrue(2 in self.range_queue)
+        self.assertTrue(3 in self.range_queue)
+        self.assertFalse(-1 in self.range_queue)
+        self.assertFalse(4 in self.range_queue)
+        self.assertFalse('0' in self.range_queue)
+        self.assertFalse('1' in self.range_queue)
+        self.assertFalse('2' in self.range_queue)
+        self.assertFalse('3' in self.range_queue)
+        self.assertTrue(-3 in self.queue)
+        self.assertTrue(1 in self.queue)
+        self.assertTrue(2 in self.queue)
+        self.assertTrue(42 in self.queue)
+        self.assertFalse(0 in self.queue)
+        self.assertFalse('-3' in self.queue)
+        self.assertFalse('1' in self.queue)
+        self.assertFalse('2' in self.queue)
+        self.assertFalse('42' in self.queue)
+
+    def test_iadd(self):
+        with self.assertRaises(TypeError):
+            self.empty_queue += 2
+        with self.assertRaises(TypeError):
+            self.queue_length_1 += 2.5
+        with self.assertRaises(TypeError):
+            self.range_queue += True
+        with self.assertRaises(TypeError):
+            self.queue += None
+
+        id_empty_queue = id(self.empty_queue)
+        id_queue_length_1 = id(self.queue_length_1)
+        id_range_queue = id(self.range_queue)
+        id_queue = id(self.queue)
+
+        self.empty_queue += self.queue_length_1
+        self.queue_length_1 += self.range_queue
+        self.range_queue += self.queue
+        self.queue += self.empty_queue
+
+        self.assertEqual(id(self.empty_queue), id_empty_queue)
+        self.assertEqual(id(self.queue_length_1), id_queue_length_1)
+        self.assertEqual(id(self.range_queue), id_range_queue)
+        self.assertEqual(id(self.queue), id_queue)
+
+        queue = self.tested_class()
+        queue += [0]
+        self.assertEqual(self.empty_queue, queue)
+        queue = self.tested_class()
+        queue += [0, 0, 1, 2, 3]
+        self.assertEqual(self.queue_length_1, queue)
+        queue = self.tested_class()
+        queue += [0, 1, 2, 3, 1, 42, -3, 2, 42]
+        self.assertEqual(self.range_queue, queue)
+        queue = self.tested_class()
+        queue += [1, 42, -3, 2, 42, 0]
+        self.assertEqual(self.queue, queue)
+
+        self.empty_queue += [-1, -1]
+        self.queue_length_1 += tuple('queue')
+        self.range_queue += (-1, -2, -3)
+        self.queue += []
+
+        self.assertEqual(id(self.empty_queue), id_empty_queue)
+        self.assertEqual(id(self.queue_length_1), id_queue_length_1)
+        self.assertEqual(id(self.range_queue), id_range_queue)
+        self.assertEqual(id(self.queue), id_queue)
+        queue = self.tested_class()
+        queue += [0, -1, -1]
+        self.assertEqual(self.empty_queue, queue)
+        queue = self.tested_class()
+        queue += [0, 0, 1, 2, 3, 'q', 'u', 'e', 'u', 'e']
+        self.assertEqual(self.queue_length_1, queue)
+        queue = self.tested_class()
+        queue += [0, 1, 2, 3, 1, 42, -3, 2, 42, -1, -2, -3]
+        self.assertEqual(self.range_queue, queue)
+        queue = self.tested_class()
+        queue += [1, 42, -3, 2, 42, 0]
+        self.assertEqual(self.queue, queue)
+
+    def test_is_empty(self):
+        self.assertTrue(self.empty_queue.is_empty())
+        self.assertFalse(self.queue_length_1.is_empty())
+        self.assertFalse(self.range_queue.is_empty())
+        self.assertFalse(self.queue.is_empty())
+
+    def test_enqueue(self):
+        self.empty_queue.enqueue(-1)
+        self.queue_length_1.enqueue(-1)
+        self.queue_length_1.enqueue(-2)
+        self.range_queue.enqueue(-1)
+        self.range_queue.enqueue(-2)
+        self.range_queue.enqueue(-3)
+        self.queue.enqueue(-1)
+        self.queue.enqueue(-2)
+        self.queue.enqueue(-3)
+
+        queue = self.tested_class()
+        queue += [-1]
+        self.assertEqual(self.empty_queue, queue)
+        queue = self.tested_class()
+        queue += [0, -1, -2]
+        self.assertEqual(self.queue_length_1, queue)
+        queue = self.tested_class()
+        queue += [0, 1, 2, 3, -1, -2, -3]
+        self.assertEqual(self.range_queue, queue)
+        queue = self.tested_class()
+        queue += [1, 42, -3, 2, 42, -1, -2, -3]
+        self.assertEqual(self.queue, queue)
+
+
+class TestArrayRandomizedQueue(TestRandomizedQueue):
+    def __init__(self, method_name):
+        super().__init__(method_name=method_name,
+                         tested_class=ArrayRandomizedQueue)
+
+    def test_init(self):
+        self.assertEqual(self.empty_queue._values, [])
+        self.assertEqual(self.queue_length_1._values, [0])
+        self.assertEqual(self.range_queue._values, [0, 1, 2, 3])
+        self.assertEqual(self.queue._values, [1, 42, -3, 2, 42])
+
+    def test_iter(self):
+        self.assertEqual(list(iter(self.empty_queue)), [])
+        self.assertEqual(list(iter(self.queue_length_1)), [0])
+        self.assertEqual(list(iter(self.range_queue)), [2, 3, 1, 0])
+        self.assertEqual(list(iter(self.queue)), [-3, 1, 42, 2, 42])
+
+    def test_repr(self):
+        class_name = self.tested_class.__name__
+        self.assertEqual(repr(self.empty_queue), '{}([])'.format(class_name))
+        self.assertEqual(repr(self.queue_length_1),
+                         '{}([0])'.format(class_name))
+        self.assertEqual(repr(self.range_queue),
+                         '{}([0, 1, 2, 3])'.format(class_name))
+        self.assertEqual(repr(self.queue),
+                         '{}([1, 42, -3, 2, 42])'.format(class_name))
+
+    def test_str(self):
+        self.assertEqual(str(self.empty_queue), '')
+        self.assertEqual(str(self.queue_length_1), '0')
+        self.assertEqual(str(self.range_queue), '0 1 2 3')
+        self.assertEqual(str(self.queue), '1 42 -3 2 42')
+
+    def test_choice(self):
+        with self.assertRaises(EmptyRandomizedQueueException):
+            self.empty_queue.choice()
+
+        self.assertEqual(self.queue_length_1.choice(), 0)
+        self.assertEqual(self.range_queue.choice(), 3)
+        self.assertEqual(self.queue.choice(), 1)
+
+    def test_dequeue(self):
+        with self.assertRaises(EmptyRandomizedQueueException):
+            self.empty_queue.dequeue()
+
+        self.assertEqual(self.queue_length_1.dequeue(), 0)
+        self.assertEqual(self.range_queue.dequeue(), 3)
+        self.assertEqual(self.range_queue.dequeue(), 0)
+        self.assertEqual(self.queue.dequeue(), -3)
+        self.assertEqual(self.queue.dequeue(), 42)
+
+        queue = self.tested_class()
+        self.assertEqual(self.queue_length_1, queue)
+        queue = self.tested_class()
+        queue += [1, 2]
+        self.assertEqual(self.range_queue, queue)
+        queue = self.tested_class()
+        queue += [1, 2, 42]
+        self.assertEqual(self.queue, queue)
+
+        self.assertIn(self.queue.dequeue(), {1, 2, 42})
+        self.assertIn(self.queue.dequeue(), {1, 2, 42})
+        self.assertIn(self.queue.dequeue(), {1, 2, 42})
+        with self.assertRaises(EmptyRandomizedQueueException):
+            self.queue.dequeue()
+
+
+class TestLinkedRandomizedQueue(TestRandomizedQueue):
+    def __init__(self, method_name):
+        super().__init__(method_name=method_name,
+                         tested_class=LinkedRandomizedQueue)
+
+    def test_init(self):
+        self.assertEqual(self.empty_queue._front, None)
+
+        self.assertEqual(self.queue_length_1._front.value, 0)
+        self.assertEqual(self.queue_length_1._front.successor, None)
+
+        self.assertEqual(self.range_queue._front.value, 3)
+        self.assertEqual(self.range_queue._front.successor.value, 2)
+        self.assertEqual(self.range_queue._front.successor.successor.value, 1)
+        self.assertEqual(self.range_queue._front.successor.successor.successor
+                         .value, 0)
+        self.assertEqual(self.range_queue._front.successor.successor.successor
+                         .successor, None)
+
+        self.assertEqual(self.queue._front.value, 42)
+        self.assertEqual(self.queue._front.successor.value, 2)
+        self.assertEqual(self.queue._front.successor.successor.value, -3)
+        self.assertEqual(self.queue._front.successor.successor.successor.value,
+                         42)
+        self.assertEqual(self.queue._front.successor.successor.successor
+                         .successor.value, 1)
+        self.assertEqual(self.queue._front.successor.successor.successor
+                         .successor.successor, None)
+
+    def test_iter(self):
+        self.assertEqual(list(iter(self.empty_queue)), [])
+        self.assertEqual(list(iter(self.queue_length_1)), [0])
+        self.assertEqual(list(iter(self.range_queue)), [1, 0, 2, 3])
+        self.assertEqual(list(iter(self.queue)), [-3, 42, 2, 42, 1])
+
+    def test_repr(self):
+        class_name = self.tested_class.__name__
+        self.assertEqual(repr(self.empty_queue), '{}([])'.format(class_name))
+        self.assertEqual(repr(self.queue_length_1),
+                         '{}([0])'.format(class_name))
+        self.assertEqual(repr(self.range_queue),
+                         '{}([3, 2, 1, 0])'.format(class_name))
+        self.assertEqual(repr(self.queue),
+                         '{}([42, 2, -3, 42, 1])'.format(class_name))
+
+    def test_str(self):
+        self.assertEqual(str(self.empty_queue), '')
+        self.assertEqual(str(self.queue_length_1), '0')
+        self.assertEqual(str(self.range_queue), '3 2 1 0')
+        self.assertEqual(str(self.queue), '42 2 -3 42 1')
+
+    def test_choice(self):
+        with self.assertRaises(EmptyRandomizedQueueException):
+            self.empty_queue.choice()
+
+        self.assertEqual(self.queue_length_1.choice(), 0)
+        self.assertEqual(self.range_queue.choice(), 0)
+        self.assertEqual(self.queue.choice(), 42)
+
+    def test_dequeue(self):
+        with self.assertRaises(EmptyRandomizedQueueException):
+            self.empty_queue.dequeue()
+
+        self.assertEqual(self.queue_length_1.dequeue(), 0)
+        self.assertEqual(self.range_queue.dequeue(), 0)
+        self.assertEqual(self.range_queue.dequeue(), 3)
+        self.assertEqual(self.queue.dequeue(), -3)
+        self.assertEqual(self.queue.dequeue(), 1)
+
+        queue = self.tested_class()
+        self.assertEqual(self.queue_length_1, queue)
+        queue = self.tested_class()
+        queue += [1, 2]
+        self.assertEqual(self.range_queue, queue)
+        queue = self.tested_class()
+        queue += [2, 42, 42]
+        self.assertEqual(self.queue, queue)
+
+        self.assertIn(self.queue.dequeue(), {2, 42})
+        self.assertIn(self.queue.dequeue(), {2, 42})
+        self.assertIn(self.queue.dequeue(), {2, 42})
+        with self.assertRaises(EmptyRandomizedQueueException):
+            self.queue.dequeue()
+
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+
+    # add test methods as separate tests to test suite
+    for test_case in [TestArrayRandomizedQueue, TestLinkedRandomizedQueue]:
+        for name in unittest.defaultTestLoader.getTestCaseNames(test_case):
+            suite.addTest(test_case(name))
+
+    # run the tests
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
