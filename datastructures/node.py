fdeb5dfae5877d24489d789ed38457ae1cb346c4
Sebastian Thomas <sigma.online@gmx.de>
2021-01-19 08:35:21 +0100

    added node class with key field

--- datastructures/node.py
+++ datastructures/node.py
@@ -2,7 +2,7 @@
 
 
 class LinkedNode:
-    """Node class for e.g. (singly) linked lists, linked stacks, ..."""
+    """Node class for eg (singly) linked lists, linked stacks, ..."""
 
     def __init__(self, value, successor=None):
         self.value = value
@@ -15,8 +15,24 @@ class LinkedNode:
         return str(self.value)
 
 
+class LinkedNodeWithKey(LinkedNode):
+    """Node class with a key field, eg for linked dictionaries."""
+
+    def __init__(self, key, value, successor=None):
+        super().__init__(value, successor=successor)
+        self.key = key
+
+    def __repr__(self):
+        return '{}(key={}, value={})'.format(type(self).__name__,
+                                             repr(self.key), repr(self.value))
+
+    def __str__(self):
+        return '({}: {})'.format(type(self).__name__, str(self.key),
+                                 str(self.value))
+
+
 class DoublyLinkedNode(LinkedNode):
-    """Node class for e.g. doubly linked lists, ..."""
+    """Node class for eg doubly linked lists, ..."""
 
     def __init__(self, value, predecessor=None, successor=None):
         super().__init__(value, successor=successor)
